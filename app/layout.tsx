0dfe11cbfd395ffee9b4307717d6a9e5f69386e7
Xuân Lâm <hoangxuanlam2007@outlook.com>
2026-02-12 21:40:24 +0700

    Add suppressHydrationWarning

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={`font-sans antialiased`}>
         {children}
         <Toaster />
